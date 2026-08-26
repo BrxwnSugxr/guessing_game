0b7541868edeba0d57d267aed01169c4120aea97
mike <jsdev.mas@gmail.com>
2026-08-26 11:03:22 +1000

    update

---
+++ quiz/assets/js/questions.js
@@ -0,0 +1,87 @@
+const questions = [
+  {
+    title:
+      "1. Which collection object allows a unique value to be inserted only once?",
+    choice: ["Object", "Set", "Array", "Map"],
+    answer: "Set",
+  },
+  {
+    title: "2. Which method do you use to attach one DOM node to another?",
+    choice: ["attachNode()", "getNode()", "querySelector()", "appendChild()"],
+    answer: "appendChild()",
+  },
+  {
+    title: "3. Which statement can be used to skip an iteration in a loop?",
+    choice: ["break", "skip", "continue", "pass"],
+    answer: "",
+  },
+  {
+    title:
+      "4. Which property references the DOM object that dispatched an event?",
+    choice: ["target", "self", "object", "source"],
+    answer: "target",
+  },
+  {
+    title: "5. Which method converts JSON data to a JavaScript object?",
+    choice: [
+      "JSON.fromString()",
+      "JSON.parse()",
+      "JSON.toObject()",
+      "JSON.stringify()",
+    ],
+    answer: "JSON.parse()",
+  },
+  {
+    title:
+      "6. Which of the following methods can be used to display data in some form using JavaScript?",
+    choice: [
+      "document.write()",
+      "console.log()",
+      "window.alert()",
+      "All of the above",
+    ],
+    answer: "All of the above",
+  },
+  {
+    title: '7. What does the JavaScript "debugger" statement do?',
+    choice: [
+      "It debugs all errors in the program at runtime",
+      "It acts as a breakpoint in a program",
+      "It debugs an error in the current statement",
+      "All of the above",
+    ],
+    answer: "It acts as a breakpoint in a program",
+  },
+  {
+    title:
+      "8. Which function is used to serialize an object into a JSON string in JavaScript?",
+    choice: [
+      "JSON.stringify()",
+      "JSON.parse()",
+      "JSON.convert()",
+      "None of the above",
+    ],
+    answer: "JSON.stringify()",
+  },
+  {
+    title: "9. How do you stop an interval timer in JavaScript?",
+    choice: [
+      "clearTimer()",
+      "clearInterval()",
+      "intervalOver()",
+      "None of the above",
+    ],
+    answer: "clearInterval()",
+  },
+  {
+    title:
+      "10. The process in which an object or data structure is translated into a format suitable for transfer or storage is called what?",
+    choice: [
+      "Object Inheritance",
+      "Object Encapsulation",
+      "Object Serialization",
+      "None of the above",
+    ],
+    answer: "Object Serialization",
+  },
+];
